330175cbb5923b037da55e5d73752c2cd80e8039
thebwood <brian.d.wood@gmail.com>
2022-01-20 23:49:08 -0500

    Adding solution with projects

---
+++ InventoryManagementServices/Services/Game/Game.API/Controllers/GameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+    }
+}
---
+++ InventoryManagementServices/Services/Movie/Movie.API/Controllers/MovieController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+    }
+}
